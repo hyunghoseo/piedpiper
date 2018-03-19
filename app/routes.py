63fb6d8496d020f19c942a0224a7483104a2a4af
Hyungho Seo <hseo@stevens.edu>
2018-03-19 14:22:51 -0400

    add a route for IDE

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,8 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/ide')
+def ide():
+    return render_template('rextester.html')
