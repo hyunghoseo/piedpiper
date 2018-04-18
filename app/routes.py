b7148e50074f8f59fdc6a99acc97848e198c823f
Hyungho Seo <hseo@stevens.edu>
2018-04-18 18:08:29 -0400

    video test

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, request, url_for, redirect, session
-from app import app
+from app import app, sio
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from pusher import Pusher
 import json, os, subprocess
 import db
+import datetime
 
 db.createdb()
 
@@ -150,3 +151,51 @@ def run_code():
     print file_id
     
     return '200' #SUCCESS
+
+
+connected_particpants = {}
+
+def write_log(s):    
+    with open('logfile.out', 'a+') as f:
+        f.write('time: %s Action: %s \n' % (str(datetime.datetime.now()), s))
+
+@app.route('/videotest/')
+def default_room():
+    """Serve index page"""
+    return render_template('videotest.html', room='default')
+
+@sio.on('message', namespace='/')
+def messgage(sid, data):
+    sio.emit('message', data=data)
+
+@sio.on('disconnect', namespace='/')
+def disconnect(sid):
+    write_log("Received Disconnect message from %s" % sid)
+    for room, clients in connected_particpants.iteritems():
+        try:
+            clients.remove(sid)
+            write_log("Removed %s from %s \n list of left participants is %s" %(sid, room, clients))
+        except ValueError:
+            write_log("Remove %s from %s \n list of left participants is %s has failed" %(sid, room, clients))
+
+@sio.on('create or join', namespace='/')
+def create_or_join(sid, data):
+    print sid, data
+    sio.enter_room(sid, data)
+    try:
+        connected_particpants[data].append(sid)
+    except KeyError:
+        connected_particpants[data] = [sid]
+    numClients = len(connected_particpants[data])
+    if numClients == 1:
+        sio.emit('created', data)
+    elif numClients > 2:
+        sio.emit('full')
+    elif numClients == 2:
+        sio.emit('joined')
+        sio.emit('join')
+    print (sid, data, len(connected_particpants[data]))
+
+@app.route('/videotest/<room>/')
+def room(room):
+    return render_template('videotest.html', room=room)
